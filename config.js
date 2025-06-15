d6a1519df102acb39368a456463bba1e0e103ba7
ThaneLiam <NguyenThaneLiam28012005@gmail.com>
2025-06-15 08:21:35 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    titleWeb: "Anh bé yêu công của Matcha Latte quãi ò",
+    introTitle: 'Em bé Matcha latte💖💚',
+    introDesc: `người tuii iu nhứt trên đờiiii 😍😘`,
+    btnIntro: 'Aiii dzayyy',
+    title: 'Hummm wa a lỡ để emm bé ngủ mò hămmm chúc ngủ ngoannn dòiiii huhu',
+    desc: 'em bé tha lỗi choa a nhooo nhooo nhooo 😘 ',
+    btnYes: 'Dạaaa chìu ông<33',
+    btnNo: 'Hong đâuuuuu :3',
+    question: 'Yeeeeee iu em bé quó trờiiii lunnn, i love you so muchhh',
+    btnReply: 'Gửi anh nhóoo',
+    reply: 'Dù em bé nói cái gì đi nữa hì anh bé vẫn iu em bé matcha latte ',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    messDesc: 'Hun mín nhoooooo',
+    btnAccept: 'dạaaaa <3',
+    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://web.facebook.com/messages/e2ee/t/8824191827683465
 }
